71f21e40d7ff4c539b5af0cc0bb6ce35160a7cd1
pau <pausoltor@gmail.com>
2023-12-04 15:02:40 +0100

    Add pokeapi package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Pokedex from 'pokedex-promise-v2';
 
 function App() {
+  const P = new Pokedex();
+  (async () => {
+    const golduck = await P.getPokemonByName("golduck")
+    console.log(golduck)
+  })()
+
   return (
     <div className="App">
       <header className="App-header">
